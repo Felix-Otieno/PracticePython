f9a411477f2e7a876d1e3a934a297cbecabd39c5
Felix-Otieno <omondifelixotieno2@gmail.com>
2024-09-19 16:45:47 +0300

    practices exercises on python

---
+++ thur12.py
@@ -0,0 +1,5 @@
+a, b = 5, 12
+if a + b:
+    print("True")
+else:
+    print("False")
---
+++ thur14.py
@@ -0,0 +1,5 @@
+numbers = [10, 20]
+items = ["chairs", "Table"]
+for x in numbers:
+    for y in items:
+        print(x, y)
